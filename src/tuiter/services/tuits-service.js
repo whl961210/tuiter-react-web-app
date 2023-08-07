e9c17530b574a7260b8cd7804c66c4bb9879e45d
whl961210 <whl961210@gmail.com>
2023-08-07 22:05:03 +0800

    updates

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+const API_BASE = process.env.REACT_APP_SERVER_API_URL;
+const TUITS_API = `${API_BASE}/tuits`;
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
